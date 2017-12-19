952305d1e096a7243cd83d86f57091c1b2200baf
Ian Munene <munene.ian@live.com>
2017-12-19 23:56:18 +0300

    version 1 of application complete

--- app/src/main/java/com/ianmunene/javalove/DrinkActivity.java
+++ app/src/main/java/com/ianmunene/javalove/DrinkActivity.java
@@ -2,6 +2,8 @@ package com.ianmunene.javalove;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends Activity {
 
@@ -11,5 +13,22 @@ public class DrinkActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        // Get the drink from the intent
+        int drinkId = (int) getIntent().getExtras().get(EXTRA_DRINKID);
+        Drink drink = Drink.drinks[drinkId];
+
+        // Populate the drink name
+        TextView name = findViewById(R.id.tv_drink_name);
+        name.setText(drink.getName());
+
+        // Populate the description
+        TextView description = findViewById(R.id.tv_description);
+        description.setText(drink.getDescription());
+
+        // Populate the drink Image
+        ImageView drinkImage = findViewById(R.id.iv_drink);
+        drinkImage.setImageResource(drink.getImageResourceId());
+        drinkImage.setContentDescription(drink.getName());
     }
 }
